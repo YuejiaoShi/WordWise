1901628d07dba41d6c6798634a526ff1fc18cda0
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-17 16:15:11 +0200

    extract the latlng property from the click event, and showing it on URL temperately

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -69,7 +69,7 @@ function DetectClick() {
   const navigate = useNavigate();
 
   useMapEvents({
-    click: (e) => navigate("form"),
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
   });
 }
 
