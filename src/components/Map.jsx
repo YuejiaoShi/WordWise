463b4362bc5189648aad57cb5670e3d21f8ddf91
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-17 16:46:22 +0200

    Extract the useSearchParams(lat,lng) part out to a new hook: useUrlPosition. make it reusable

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,6 +13,7 @@ import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
 import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
   const { cities } = useCities();
@@ -25,9 +26,7 @@ function Map() {
     getPosition,
   } = useGeolocation();
 
-  const [searchParams] = useSearchParams();
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
     function () {
