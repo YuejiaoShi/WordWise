76a696e9bcdf5d77eb997c1fc34eec4f80b66fbb
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-23 23:17:45 +0200

    debug

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -8,28 +8,45 @@ function reducer(state, action) {
   switch (action.type) {
     case "loading":
       return { ...state, isLoading: true };
+
     case "cities/loaded":
-      return { ...state, isLoading: false, currentCity: action.payload };
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+
     case "city/loaded":
       return { ...state, isLoading: false, currentCity: action.payload };
+
     case "city/created":
       return {
         ...state,
         isLoading: false,
         cities: [...state.cities, action.payload],
+        currentCity: action.payload,
       };
+
     case "city/deleted":
       return {
         ...state,
         isLoading: false,
         cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
       };
+
     case "rejected":
-      return { ...state, isLoading: false, error: action.payload };
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+
     default:
       throw new Error("Unknown action type");
   }
 }
+
 const initialState = {
   cities: [],
   isLoading: false,
