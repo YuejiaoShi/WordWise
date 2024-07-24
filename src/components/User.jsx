b03aeebc04828556bd78652b94ea6abb42e61128
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-24 22:39:50 +0200

    At USser, import logout func from fakeAuthContext, and use navigate to make user to go back to homepage when click logout button

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/fakeAuthContext";
 import styles from "./User.module.css";
 
 function User() {
-  const { user } = useAuth();
-
-  function handleClick() {}
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  function handleClick() {
+    logout();
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
