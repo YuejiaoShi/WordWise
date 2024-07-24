70244257cd192653a3171a16f4f5ca7d9ab23dc5
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-24 17:47:34 +0200

    implement Login in App and Login pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
+import Button from "../components/Button";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/fakeAuthContext";
 import styles from "./Login.module.css";
 import { useState } from "react";
 
@@ -7,10 +9,17 @@ export default function Login() {
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
+  const { login } = useAuth();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) login(email.password);
+  }
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +41,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
