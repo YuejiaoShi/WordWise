4abcea43e0f863f236b6f71c961efd358d46a488
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-24 17:32:37 +0200

    Create a simple reducer to handle the two cases: login and logout

--- src/contexts/fakeAuthContext.jsx
+++ src/contexts/fakeAuthContext.jsx
@@ -1,8 +1,26 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = { user: null, isAuthenticated: false };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
 function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
   function login(email, password) {}
   function logout() {}
 
