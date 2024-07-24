bbca448c2dd82e59d0c08d92bd302fde9e26c7fd
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-24 17:40:40 +0200

    finish functions login and logout by using a temporary FAKE_USER

--- src/contexts/fakeAuthContext.jsx
+++ src/contexts/fakeAuthContext.jsx
@@ -15,14 +15,26 @@ function reducer(state, action) {
   }
 }
 
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
 function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
-  function login(email, password) {}
-  function logout() {}
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+  function logout() {
+    dispatch({ type: "logout" });
+  }
 
   return (
     <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
@@ -36,3 +48,5 @@ function useAuth() {
   if (context === undefined)
     throw new Error("AuthContext was used outside AuthProvider");
 }
+
+export { AuthProvider, useAuth };
