34c2d35d838937c9232960fc0133471cb02d6604
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-26 20:55:11 +0200

    mofidy fake user info

--- src/contexts/fakeAuthContext.jsx
+++ src/contexts/fakeAuthContext.jsx
@@ -20,7 +20,7 @@ const FAKE_USER = {
   name: "Yuejiao",
   email: "yuejiao@example.com",
   password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
+  avatar: "https://i.pravatar.cc/100?img=31",
 };
 
 function AuthProvider({ children }) {
